d2761810a2aa2a8f2d860ab747ee951729e2e33d
Ariel Souza <arielsvadev@gmail.com>
2025-12-18 05:34:30 -0300

    feat(app): add MVC configuration.

---
+++ src/WebCommerce.App/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebCommerce.App.ViewModels;
+
+
+namespace WebCommerce.App.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+        public DbSet<WebCommerce.App.ViewModels.ProductViewModel> ProductViewModel { get; set; } = default!;
+    }
+}
--- src/WebCommerce.App/Program.cs
+++ src/WebCommerce.App/Program.cs
@@ -1,10 +1,13 @@
-using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using WebCommerce.App.Data;
 using WebCommerce.Business.Interfaces;
 using WebCommerce.Data.Context;
 using WebCommerce.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<DataDbContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -16,6 +19,25 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProviderRepository, ProviderRepository>();
 builder.Services.AddScoped<IAddressRepository, AddressRepository>();
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
