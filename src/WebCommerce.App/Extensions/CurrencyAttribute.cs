0cd73cde489b5cd4be1233b5c562ea35189ba410
Ariel Souza <arielsvadev@gmail.com>
2026-01-13 06:23:52 -0300

    feat(app): adjust Product view.

--- src/WebCommerce.App/Extensions/CurrencyAttribute.cs
+++ src/WebCommerce.App/Extensions/CurrencyAttribute.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.Extensions.Localization;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
 using System.Globalization;
 
 namespace WebCommerce.App.Extensions
