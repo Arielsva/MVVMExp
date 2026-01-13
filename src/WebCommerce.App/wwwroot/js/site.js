7c38dfc2a087c1a83a76615ea584a4d20369fb6a
Ariel Souza <arielsvadev@gmail.com>
2026-01-13 06:55:17 -0300

    feat(app): add validation and search by zip code.

--- src/WebCommerce.App/wwwroot/js/site.js
+++ src/WebCommerce.App/wwwroot/js/site.js
@@ -38,3 +38,55 @@ function bindForm(dialog) {
         return false;
     });
 }
+
+function SearchZipCode() {
+    $(document).ready(function () {
+
+        function clear_zip_form() {
+            $("#Address_Street").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        $("#Address_PostalCode").blur(function () {
+
+            var zip = $(this).val().replace(/\D/g, '');
+
+            if (zip != "") {
+
+                var validzipcode = /^[0-9]{8}$/;
+
+                if (validzipcode.test(zip)) {
+
+                    $("#Address_Street").val("...");
+                    $("#Address_Neighborhood").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + zip + "/json/?callback=?",
+                        function (data) {
+
+                            if (!("erro" in data)) {
+                                $("#Address_Street").val(data.logradouro);
+                                $("#Address_Neighborhood").val(data.bairro);
+                                $("#Address_City").val(data.localidade);
+                                $("#Address_State").val(data.uf);
+                            } 
+                            else {
+                                clear_zip_form();
+                                alert("ZIP code not found.");
+                            }
+                        });
+                } 
+                else {
+                    clear_zip_form();
+                    alert("Invalid ZIP code format.");
+                }
+            } 
+            else {
+                clear_zip_form();
+            }
+        });
+    });
+}
